56e82e035ceaadd60ffc707e63bcffda961f0a57
Maksim Kostromin <daggerok@gmail.com>
2020-01-04 03:07:27 +0200

    Init consumer Travis CI pipeline.

--- maven-rest-mvc-functional-producer/src/test/java/com/github/daggerok/mavenrestmvcproducer/RestBase.java
+++ maven-rest-mvc-functional-producer/src/test/java/com/github/daggerok/mavenrestmvcproducer/RestBase.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(classes = MavenRestMvcProducerApplication.class)
-class RestBase {
+abstract class RestBase {
 
     @Autowired
     private WebApplicationContext context;
